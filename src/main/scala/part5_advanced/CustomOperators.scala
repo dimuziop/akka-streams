798dc713eb7add6704759e880ed21479a26edf7e
FortuneTeller <dimuziop@gmail.com>
2021-09-10 15:08:04 +0200

    finished

--- src/main/scala/part5_advanced/CustomOperators.scala
+++ src/main/scala/part5_advanced/CustomOperators.scala
@@ -1,12 +1,13 @@
 package part5_advanced
 
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Sink, Source}
-import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
-import akka.stream.{Attributes, Inlet, Materializer, Outlet, SinkShape, SourceShape}
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
+import akka.stream.stage.{GraphStage, GraphStageLogic, GraphStageWithMaterializedValue, InHandler, OutHandler}
+import akka.stream.{Attributes, FlowShape, Inlet, Materializer, Outlet, SinkShape, SourceShape}
 
 import scala.collection.mutable
-import scala.util.Random
+import scala.concurrent.{Future, Promise}
+import scala.util.{Failure, Random, Success}
 
 /**
  * User: patricio
@@ -91,5 +92,110 @@ object CustomOperators extends App {
   }
 
   val batcherSink = Sink.fromGraph(new Batcher(50))
-  randomGeneratorSource.to(batcherSink).run()
+  //randomGeneratorSource.to(batcherSink).run()
+
+  /**
+   * Exercise: a custom flow - a simple filter flow
+   * - 2 ports: an input port and an ourput port
+   */
+
+  class FilterFlow[S](predicate: S => Boolean) extends GraphStage[FlowShape[S,S]] {
+
+    val outPort = Outlet[S]("outputFilterFlow")
+    val inPort = Inlet[S]("inputInletFlow")
+
+    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
+
+
+      setHandler(outPort, new OutHandler {
+        override def onPull(): Unit = {
+          pull(inPort)
+        }
+      })
+      setHandler(inPort, new InHandler {
+        override def onPush(): Unit = {
+          try {
+            val element = grab(inPort)
+            if (predicate(element)) {
+              push(outPort, element)
+            } else {
+              pull(inPort)
+            }
+          } catch {
+            case e: Throwable => failStage(e)
+          }
+        }
+
+      })
+    }
+
+    override def shape: FlowShape[S, S] = FlowShape[S, S](inPort, outPort)
+  }
+
+  val filterFlow = Flow.fromGraph(new FilterFlow[Int](_ >= 50))
+
+  //randomGeneratorSource.via(filterFlow).to(batcherSink).run()
+
+  /*
+  Materialized values in graph stages
+   */
+  // 3 - flow that counts the number of elements that goes through it
+
+  class CounterFlow[T] extends GraphStageWithMaterializedValue[FlowShape[T, T], Future[Int]] {
+    val outPort = Outlet[T]("counterIn")
+    val inPort = Inlet[T]("counterOut")
+
+    override def createLogicAndMaterializedValue(inheritedAttributes: Attributes): (GraphStageLogic, Future[Int]) = {
+      val promise = Promise[Int]
+      val logic: GraphStageLogic = new GraphStageLogic(shape) {
+        var counter = 0
+        setHandler(outPort, new OutHandler {
+          override def onPull(): Unit = {
+            pull(inPort)
+          }
+
+          override def onDownstreamFinish(): Unit = {
+            promise.success(counter)
+            super.onDownstreamFinish()
+          }
+        })
+        setHandler(inPort, new InHandler {
+          override def onPush(): Unit = {
+            try {
+              val element = grab(inPort)
+              counter +=1
+              push(outPort, element)
+            } catch {
+              case e: Throwable => failStage(e)
+            }
+          }
+
+          override def onUpstreamFinish(): Unit = {
+            promise.success(counter)
+            super.onUpstreamFinish()
+          }
+
+          override def onUpstreamFailure(ex: Throwable): Unit = {
+            promise.failure(ex)
+            super.onUpstreamFailure(ex)
+          }
+        })
+      }
+      (logic, promise.future)
+    }
+
+    override val shape: FlowShape[T, T] = FlowShape(inPort, outPort)
+  }
+
+  val counterFlow = Flow.fromGraph(new CounterFlow[Int])
+
+  val countFuture = Source(1 to 10).viaMat(counterFlow)(Keep.right).to(Sink.foreach[Int](println)).run()
+
+  import system.dispatcher
+
+  countFuture.onComplete {
+    case Success(value) => println(s"Quantiyty is $value")
+    case Failure(exception) => println(s"Error is: $exception")
+  }
+
 }
